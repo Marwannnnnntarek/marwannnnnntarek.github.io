267bc4fabe6ed9e77f850edcbf0bef823ef42c2d
Marwan Tarek <119248877+Marwannnnnntarek@users.noreply.github.com>
2026-02-07 13:57:40 +0200

    index html locatioon

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
 import Resume from "./components/Resume/ResumeNew";
 import {
-  k
+
   BrowserRouter as Router,
   Route,
   Routes,
